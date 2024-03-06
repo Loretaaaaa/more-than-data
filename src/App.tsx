b52c624485a4f8f1c6cdc1a55f5a329882b5e003
Loreta <badalyan.loreta@gmail.com>
2024-03-06 13:26:52 +0400

    used Async/Await

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -8,15 +8,34 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   //get -> promise -> res / err
+  //   axios
+  //     .get<User[]>("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => setUsers(res.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
-  }, []);
+    const fetchUsers = async () => {
+      try {
+        const res = await axios
+          .get<User[]>('https://jsonplaceholder.typicode.com/users');
+        setUsers(res.data);
+      }
+      catch (err) {
+        setError((err as AxiosError).message);
+      }
+    }
+    fetchUsers();
+
+  }, [])
 
   return (
     <>
+      {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
